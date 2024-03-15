73948844b4b0d234dd33e25f0f873446509bd313
Thomas Boerger <thomas@webhippie.de>
2024-03-15 14:56:38 +0100

    feat: integrate a more complex default config

---
+++ latest/overlay/etc/commitlint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    extends: [
+        "/usr/local/lib/node_modules/@commitlint/config-conventional"
+    ],
+    rules: {
+        'type-enum': [
+            'build',
+            'chore',
+            'ci',
+            'docs',
+            'feat',
+            'fix',
+            'perf',
+            'refactor',
+            'revert',
+            'style',
+            'test',
+            'deps',
+        ]
+    }
+};
